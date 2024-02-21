adc787c030768cc19010c3a42806587228512146
Isaiah Mayerchak <mayeis01@yahoo.com>
2024-02-21 14:34:53 -0800

    start adding navbar

--- client/src/Components/Home/Home.tsx
+++ client/src/Components/Home/Home.tsx
@@ -1,26 +1,29 @@
 import Grid from '@mui/material/Grid';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import Anime from '../../Models/anime';
 import CatalogPane from './CatalogPane/CatalogPane';
 import CurrentlySelectedPane from './CurrentlySelectedPane/CurrentlySelectedPane';
 import FinalistsPane from './FinalistsPane/FinalistsPane';
+import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
-
     const [currentlySelected, setCurrentlySelected] = useState<Anime>();
 
     return (
-        <Grid container justifyContent="space-around">
-            <Grid item xs={3}>
-                <CatalogPane setCurrentlySelected={setCurrentlySelected} />
+        <Fragment>
+            <Navbar></Navbar>
+            <Grid container justifyContent="space-around">
+                <Grid item xs={3}>
+                    <CatalogPane setCurrentlySelected={setCurrentlySelected} />
+                </Grid>
+                <Grid item xs={4}>
+                    <CurrentlySelectedPane currentlySelected={currentlySelected} />
+                </Grid>
+                <Grid item xs={3}>
+                    <FinalistsPane />
+                </Grid>
             </Grid>
-            <Grid item xs={4}>
-                <CurrentlySelectedPane currentlySelected={currentlySelected} />
-            </Grid>
-            <Grid item xs={3}>
-                <FinalistsPane />
-            </Grid>
-        </Grid>
+        </Fragment>
     );
 }
 
---
+++ client/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useTheme } from "@mui/material";
+import AuthContext from "../../Store/AuthContext";
+import styles from './Navbar.module.css';
+
+const Header: React.FC<{}> = (props) => {
+
+    // Setup
+    const authContext = useContext(AuthContext);
+    const theme = useTheme();
+
+    // Fetch
+
+    useEffect(() => {
+
+    }, []);
+
+    // JSX
+
+    var listSX = {
+        maxHeight: "700px",
+        overflow: "auto",
+        paddingTop: "0px"
+    }
+
+    var subheaderSX = {
+        border: "1px solid #c3c3c3",
+        borderWidth: "2px",
+        lineHeight: "30px"
+    }
+
+    return (
+    <div className={styles["search-toolbar"]} style={{"backgroundColor": theme.palette.primary.main}}>
+        <div className={styles["search-toolbar-inner-container"]}>
+            TEST
+        </div>
+    </div>
+    );
+}
+
+export default Header;
